e42561d7327d6539bd9a4669bb137074f9ce2cc8
FdezDev <fdeznet224@gmail.com>
2026-05-19 22:36:20 -0600

    May 19 10:36

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register' // 👈 Importante
 import './index.css'
 import App from './App.tsx'
 
+// 1. Registro del Service Worker
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('Nueva versión disponible. ¿Recargar para actualizar?')) {
+      window.location.reload();
+    }
+  },
+  onOfflineReady() {
+    console.log('FdezNet listo para trabajar offline');
+  },
+})
+
+// 2. Renderizado de la App
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
